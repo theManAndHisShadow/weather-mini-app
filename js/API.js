334a3ba6e16a8f39a80b0c1deea9b862c0c5b93a
plvs vltra <justemailpostbox@gmail.com>
2023-01-22 14:13:56 +0500

    Changed API.weather.now() response format

--- js/API.js
+++ js/API.js
@@ -41,7 +41,15 @@ const API = {
                 fetch(`${this.url}?lat=${ciyLat}&lon=${cityLon}&limit=5&appid=${API.key}&units=${this.units}`)
                      .then(response => response.json())
                      .then(response => {
-                        resolve(response);
+                        resolve({
+                            country: response.sys.country,
+                            state: response.state,
+                            city: response.name,
+                            coordinates: response.coord,
+                            temp: response.main.temp,
+                            temp_feels_like: response.main.feels_like,
+                            description: response.weather[0].description,
+                        });
                      });
             });
         }
